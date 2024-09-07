fafe81ec6928becb2d229e30e8666cfef12fb74d
afaq-full-stack <afaq.dev.js@gmail.com>
2024-09-07 23:17:20 +0500

    added header manually

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,13 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://advanced-auth-mern-dyw1-7gcu12mim-afaq-ahmads-projects-571f8223.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 // if(process.env.NODE_ENV === "production"){
 //     app.use(express.static(path.join(__dirname, "/frontend/dist")));
