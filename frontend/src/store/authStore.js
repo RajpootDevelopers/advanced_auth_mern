38fdcadb42e47ad72484543bcfaaf0a99831c00b
afaq-full-stack <afaq.dev.js@gmail.com>
2024-09-07 18:42:31 +0500

    corrected env variable name is Vite_BACKEND_URL

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import axios from "axios";
 
 // const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
-const API_URL = import.meta.env.BACKEND_URL;
+// const API_URL = import.meta.env.BACKEND_URL;
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set)=> ({
 
