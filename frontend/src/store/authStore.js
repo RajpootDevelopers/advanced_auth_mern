e0d4f386a4e1720a3eb4602a26f9ad21daf2a301
afaq-full-stack <afaq.dev.js@gmail.com>
2024-09-07 16:53:37 +0500

    adding backend url in env and related files

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+// const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.BACKEND_URL;
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set)=> ({
 
