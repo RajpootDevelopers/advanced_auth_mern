9a4cc0f26b91b65978e868a93b1beb69b578ea69
afaq-full-stack <afaq.dev.js@gmail.com>
2024-09-07 23:09:56 +0500

    adf

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -4,8 +4,7 @@ import axios from "axios";
 // const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 // const API_URL = import.meta.env.BACKEND_URL;
 // const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
-// const API_URL = 'https://advanced-auth-mern-backend.vercel.app/api/auth'
-const API_URL = '/api/auth'
+const API_URL = 'https://advanced-auth-mern-backend.vercel.app/api/auth'
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set)=> ({
 
