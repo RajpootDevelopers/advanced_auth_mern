5812d7233f807fb8ed4f44fc18cc97562670fbf8
afaq-full-stack <afaq.dev.js@gmail.com>
2024-09-07 16:39:30 +0500

    seprate frontend and backend for deployment on vercel

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,17 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//     app.get("*", (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//     })
+// }
 
 
 app.listen(PORT, () =>{
